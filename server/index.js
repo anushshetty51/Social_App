031504656cef98fe9f82eb7b3e7cf42510426137
anushshetty51 <anushshetty51@gmail.com>
2024-07-14 19:30:20 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv'
+
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js'
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use('/user',userRoutes);
+
+// MongoDB connection setup
+// const CONNECTION_URL = 'mongodb+srv://anushshetty:anushshetty123@cluster0.a0eyskz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    })
+    .catch((error) => console.error(error));
+
+// Uncomment the following line if you want to avoid using deprecated findAndModify()
+// mongoose.set('useFindAndModify', false);
